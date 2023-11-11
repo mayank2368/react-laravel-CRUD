d03fff63968bef7bf263d914b3e2b94c5198a295
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-11-11 23:14:42 +0100

    Access multiple layouts

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,7 +1,50 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/login";
+import SignUp from "./pages/signUp";
+import Users from "./pages/users";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import NotFound from "./pages/NotFound";
+import Dashboard from "./pages/Dashboard";
 
+const router = createBrowserRouter([
+    {
+        parth: "/",
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Navigate to="/users" />,
+            },
+            {
+                path: "/users",
+                element: <Users />,
+            },
+            {
+                path: "/dashboard",
+                element: <Dashboard />,
+            },
+        ],
+    },
+    {
+        parth: "guestlayout",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <SignUp />,
+            },
+        ],
+    },
 
-const router = createBrowserRouter(routes: [])
-
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+]);
 
 export default router;
