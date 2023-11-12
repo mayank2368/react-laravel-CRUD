ca62bb093967a244371bf4686cb1e274cbdf34e0
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-11-12 21:56:06 +0100

    Working with the context API

---
+++ react/src/context/ContextProvider.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+import { createContext, useContext, useState } from "react";
+
+const StateContext = createContext({
+    user: null,
+    token: null,
+    setUser: () => {},
+    setToken: () => {},
+});
+
+export const ContextProvider = ({ children }) => {
+    const [user, setUser] = useState({});
+    const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+
+    const setToken = (newToken) => {
+        _setToken(newToken);
+        if (newToken) {
+            localStorage.setItem("ACCESS_TOKEN", newToken);
+        } else {
+            localStorage.removeItem("ACCESS_TOKEN");
+        }
+    };
+
+    return (
+        <StateContext.Provider
+            value={{
+                user,
+                token,
+                setUser,
+                setToken,
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    );
+};
+
+ContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export const UseStateContext = () => useContext(StateContext);
