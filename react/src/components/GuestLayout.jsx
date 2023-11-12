91ec303c9fce28c58564d08794aa8d76a163ac8e
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-11-12 22:34:16 +0100

    Created proteted routes for both Default and Guest pages, also added css along with the default page UI

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { UseStateContext } from "../context/ContextProvider";
 
 export default function GuestLayout() {
+    const { token } = UseStateContext();
+    if (token) {
+        return <Navigate to="/" />;
+    }
     return (
         <div>
             <Outlet />
