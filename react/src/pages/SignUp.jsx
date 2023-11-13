b771c9220ddf7cb634808e5395d3eadcbba67349
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-11-13 22:16:13 +0100

    Created Login and sign up pages

--- react/src/pages/SignUp.jsx
+++ react/src/pages/SignUp.jsx
@@ -1,5 +1,25 @@
-function SignUp() {
-    return <div>SignUp</div>;
-}
+import { Link } from "react-router-dom";
 
-export default SignUp;
+export default function SignUp() {
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Register a new account</h1>
+                    <input type="text" placeholder="First & Last name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Re-Enter password" />
+                    <button className="btn btn-block">Register</button>
+                    <p className="message">
+                        Already have an account?{" "}
+                        <Link to="/login">Login here!</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
+}
