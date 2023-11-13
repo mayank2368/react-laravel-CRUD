ace87a747a40bb4cd63583411e4583d8ed194b02
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-11-13 22:56:02 +0100

    Implemented signup page with useRef() along with the payloads for unnecessary re-rendering

--- react/src/pages/SignUp.jsx
+++ react/src/pages/SignUp.jsx
@@ -1,18 +1,59 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { UseStateContext } from "../context/ContextProvider";
 
 export default function SignUp() {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+
+    const { setUser, setToken } = UseStateContext();
+
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        };
+
+        axiosClient
+            .post("/signup", payload)
+            .then(({ data }) => {
+                setToken(data.token);
+                setUser(data.user);
+            })
+            .catch((err) => {
+                const response = err.response;
+                if (response && response.status === 422)
+                    console.log(response.data.errors);
+            });
     };
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Register a new account</h1>
-                    <input type="text" placeholder="First & Last name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Re-Enter password" />
+                    <input
+                        ref={nameRef}
+                        type="text"
+                        placeholder="First & Last name"
+                    />
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        ref={passwordConfirmationRef}
+                        type="password"
+                        placeholder="Re-enter password"
+                    />
                     <button className="btn btn-block">Register</button>
                     <p className="message">
                         Already have an account?{" "}
